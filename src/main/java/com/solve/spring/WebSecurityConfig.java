a3f40dd5753b2c9c5b33b79e11ce9c828d7e5a3a
flwkemd <flwkemd@naver.com>
2019-02-03 01:54:44 +0900

    implemented it

--- src/main/java/com/solve/spring/WebSecurityConfig.java
+++ src/main/java/com/solve/spring/WebSecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.header.writers.frameoptions.WhiteListedAllowFromStrategy;
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
